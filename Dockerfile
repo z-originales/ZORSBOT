7b67e804b338707d3efe352f606ca2985187b6a9
ZEN <41087061+Lolozendev@users.noreply.github.com>
2024-07-20 02:13:36 +0200

    Refactored logger handler [works like a charm ;)] : - Trace now exist - Messages are unescaped in log files (easier to grep) - It has pretty colors in the terminal
    Refactored docker & compose :
    - Simple startup for the bot , no discord connectivity
    
    Switched to poetry for dependency management

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,18 @@ FROM python:3.12.2-slim-bookworm
 LABEL authors="lolozen"
 
 WORKDIR /app
-RUN useradd -r -d /app -s /bin/false bot
-RUN chown -R bot:bot /app
-USER bot
-COPY *.py ./
-COPY requirements.txt ./
-RUN pip install --no-cache-dir -r requirements.txt
-RUN rm requirements.txt
+
+RUN pip install poetry
+RUN useradd -r -d /app -s /bin/false bot && chown -R bot:bot /app
+
+# USER bot
+
 RUN mkdir -p /app/logs
-ENTRYPOINT ["python", "main.py"]
+RUN mkdir -p /app/cogs
+
+COPY *.py ./
+COPY pyproject.toml ./
+
+RUN poetry install
+
+ENTRYPOINT ["poetry", "run", "python", "main.py"]
---
+++ logger.py
@@ -0,0 +1,107 @@
+import logging
+from logging.handlers import WatchedFileHandler
+from colorlog import ColoredFormatter
+
+
+class UnescapedFormatter(logging.Formatter):
+
+    def __init__(self, fmt=None, datefmt=None, style='%'):
+        super().__init__(fmt=fmt, datefmt=datefmt, style=style)
+
+    def format(self, record):
+        record.msg = repr(record.getMessage())
+        return super().format(record)
+
+
+class LoggerHandler(logging.Logger):
+    _instance = None
+    TRACE = 5
+    logging.addLevelName(TRACE, "TRACE")
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(LoggerHandler, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self, name, log_dir, console_level=logging.INFO):
+
+        if not hasattr(self, 'is_initialized'):
+            super().__init__(name)
+            self.setLevel(console_level)
+            self.datefmt = "[%d/%m/%Y %H:%M:%S]"
+            self.add_console_handler(console_level)
+            self.add_file_handlers(log_dir)
+            self.is_initialized = True
+
+    def add_console_handler(self, console_level):
+        console_handler = logging.StreamHandler()
+        console_formatter = ColoredFormatter(
+            "%(white)s%(asctime)s %(log_color)s%(levelname)-8s%(reset)s : %(white)s%(message)s",
+            datefmt=self.datefmt,
+            reset=True,
+            log_colors={
+                'TRACE': 'fg_bold_blue,bg_white',
+                'DEBUG': 'light_purple',
+                'INFO': 'cyan',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'fg_bold_red,bg_yellow'
+            },
+        )
+        console_handler.setFormatter(console_formatter)
+        console_handler.setLevel(console_level)
+        self.addHandler(console_handler)
+
+    def add_file_handlers(self, log_dir):
+        error_file_handler = self.create_file_handler(f"{log_dir}/errors.log", logging.WARNING)
+        info_file_handler = self.create_file_handler(f"{log_dir}/info.log", logging.DEBUG, less_than=True)
+        self.addHandler(error_file_handler)
+        self.addHandler(info_file_handler)
+
+    def create_file_handler(self, file_path, level, less_than=False):
+        file_handler = WatchedFileHandler(file_path)
+        formatter = UnescapedFormatter("%(asctime)s - %(levelname)s - %(message)s", datefmt=self.datefmt)
+        file_handler.setFormatter(formatter)
+        if less_than:
+            file_handler.addFilter(lambda record: record.levelno < level)
+        else:
+            file_handler.addFilter(lambda record: record.levelno >= level)
+        return file_handler
+
+    # Utility methods
+
+    def set_level(self, level):
+        """
+        Set the logging level for both the logger and the console handler.
+
+        Args:
+            level (int): The logging level (e.g., logging.DEBUG, logging.INFO).
+        """
+        self.setLevel(level)
+        for handler in self.handlers:
+            if isinstance(handler, logging.StreamHandler):
+                handler.setLevel(level)
+
+    def set_debug_level(self):
+        """
+        Set the logging level to DEBUG for both the logger and the console handler.
+        """
+        self.set_level(logging.DEBUG)
+
+    def set_trace_level(self):
+        """
+        Set the logging level to TRACE for both the logger and the console handler.
+        """
+        self.set_level(self.TRACE)
+
+    # Custom log methods
+
+    def trace(self, msg, *args, **kwargs):
+        """
+        Log a message with severity 'TRACE' on this logger.
+
+        Args:
+            msg (str): The message to log.
+        """
+        if self.isEnabledFor(self.TRACE):
+            self._log(self.TRACE, msg, args, **kwargs)
