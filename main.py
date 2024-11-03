029d0f0a681a636145a07f105f85f1eff706f55c
ZEN <41087061+Lolozendev@users.noreply.github.com>
2024-11-03 21:38:27 +0100

    pushing data before any loss

--- main.py
+++ main.py
@@ -1,17 +1,82 @@
-from classes.logger import LoggerHandler
-from time import sleep
+from typing import Any
 
-if __name__ == '__main__':
-    logger = LoggerHandler("discord", "logs")
+import discord
+from discord.ext import commands
 
-    logger.info("This is an info message")
-    logger.warning("This is a warning message")
-    logger.error("This is an error message" + "\n\t" + 'a' * 10)
-    logger.critical("This is a critical message")
+from utilities import logger, utils
 
-    logger.set_trace_level()
 
-    while True:
-        logger.debug("This is a debug message")
-        logger.trace("This is a trace message" + "\n\t" + "5 minutes till next loop")
-        sleep(300)
+class ZORS(commands.Bot):
+
+    def __init__(self, *args, **kwargs):
+        log.debug("ZORS bot is starting up...")
+        super().__init__(*args, **kwargs)
+        self.logger = log
+        self.envs = env_vars
+        log.trace("ZORS bot has been initialized.")
+        log.debug("Loading cogs...")
+        self._load_cogs()
+
+    async def on_ready(self):
+        """
+        Event that is called when the bot is ready.
+        Returns:
+
+        """
+        log.debug("ZORS bot is up and ready.")
+        log.trace(f"Logged in as {self.user} ({self.user.id})")
+
+    def _load_cogs(self) -> None:
+        """
+        Loads all cogs in the cogs directory recursively.
+        python files starting with an underscore aren't started. This is default pycord behavior.
+
+        Returns:
+
+        """
+        status = self.load_extensions("cogs", recursive=True, store=True)
+        for extension in status:
+            match status[extension]:
+                case True:
+                    log.debug(f"Loaded cog: {extension}")
+                case discord.ExtensionAlreadyLoaded:
+                    log.debug(f"Cog already loaded: {extension} - {status[extension]}")
+                case discord.ExtensionNotFound:
+                    log.error(f"Failed to load cog: {extension} - {status[extension]}")
+                case discord.NoEntryPointError:
+                    log.error(f"Cog has no setup function: {extension} - {status[extension]}")
+                case discord.ExtensionFailed:
+                    log.error(f"Cog failed to load: {extension} - {status[extension]}")
+                case _:
+                    log.error(f"Unknown error: {extension} - {status[extension]}")
+
+
+if __name__ == "__main__":
+    log = logger.LoggerHandler("discord", "logs")
+    log.set_trace_level()
+    try:
+        env_vars = utils.get_required_env_vars()
+    except EnvironmentError as e:
+        log.critical(f"Failed to start the bot: {e}")
+        exit(1)
+
+    zorsintents = discord.Intents.none()
+    zorsintents.members = True
+    zorsintents.guilds = True
+    zorsintents.guild_messages = True
+    zorsintents.bans = True
+    zorsintents.emojis_and_stickers = True
+    zorsintents.webhooks = True
+    zorsintents.messages = True
+    zorsintents.message_content = True
+    zorsintents.reactions = True
+    zorsintents.auto_moderation_configuration = True
+    zorsintents.auto_moderation_execution = True
+
+    zors = ZORS(
+        description="ZORS !",
+        activity=discord.Game(name="/ping for now"),
+        intents=zorsintents,
+        help_command=None
+    )
+    zors.run(env_vars["DISCORD_TOKEN"])
---
+++ utilities/utils.py
@@ -0,0 +1,46 @@
+from os import getenv
+import requests
+import json
+
+
+def get_required_env_vars() -> dict:
+    """
+    Checks if specific environment variables are set and returns their values.
+    Raises an exception if any of the required environment variables are missing.
+
+    Returns:
+        dict: A dictionary of environment variable names and their values.
+    """
+    required_env_vars = [
+        "DISCORD_TOKEN",
+        "VERSION"
+    ]
+
+    env_vars_values = {}
+    missing_vars = []
+
+    for var in required_env_vars:
+        value = getenv(var)
+        if value is None:
+            missing_vars.append(var)
+        env_vars_values[var] = value
+    if missing_vars:
+        raise EnvironmentError(f"Missing required environment variables: {', '.join(missing_vars)}")
+
+    return env_vars_values
+
+
+def get_color_name(colorhex: str) -> str:
+    """
+    Returns the name of a color based on its RGB values.
+
+    Args:
+        colorhex: A string representing the hexadecimal value of a color you want to get the name of.
+
+    Returns:
+        str: The name of the color.
+    """
+    api_url = f"https://www.thecolorapi.com/id?hex={colorhex[1:]}&format=json"
+    response_json = requests.get(api_url).json()
+    return response_json["name"]["value"]
+
