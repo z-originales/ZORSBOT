b26586aacaf962e09cf852337d094159cd219c23
ZEN <41087061+Lolozendev@users.noreply.github.com>
2024-12-21 17:43:44 +0100

    added the Fun cog which is mainly used for random funny commands: - added ping to fun cog

---
+++ cogs/fun.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Context
+from loguru import logger
+
+class Fun(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command(name="ping", description="Check if the bot is alive.")
+    async def ping(self, ctx):
+        await ctx.respond(f"Pong! thanks for checking on me {ctx.author.mention} !")
+
+
+def setup(bot):
+    bot.add_cog(Fun(bot))
