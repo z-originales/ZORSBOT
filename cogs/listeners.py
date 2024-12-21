e5f65ed0b353155e939c71391511a216c6b7080a
ZEN <41087061+Lolozendev@users.noreply.github.com>
2024-12-21 19:58:05 +0100

    changed logging behavior of listeners.py cog for better syncing

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -9,7 +9,7 @@ class Listeners(commands.Cog):
 
     @discord.Cog.listener()
     async def on_application_command(self, ctx):
-        self.bot.logger.debug(
+        self.bot.logger.trace(
             f"Received command: {ctx.command} was invoked by {ctx.author}"
         )
 
@@ -20,21 +20,21 @@ class Listeners(commands.Cog):
             case commands.MissingRole:
                 missing_rolename = error.missing_role
                 roleid = str(discord.utils.get(ctx.guild.roles, name=missing_rolename).id)
-                await ctx.respond(
-                    f"You don't have the required role <@&{roleid}> to execute this command and "
-                    f"shouldn't be able to see it. Please contact an admin."
-                    , allowed_mentions=None
-                )
                 self.bot.logger.error(
                     f"{message_beginning} - MissingRole: {missing_rolename} for {ctx.author}, that shouldn't happen "
                     f"check the integration settings and make sure this command is only visible to the right role."
                 )
-            case _:
                 await ctx.respond(
-                    "An error occurred while executing the command. Please contact an admin."
+                    f"You don't have the required role <@&{roleid}> to execute this command and "
+                    f"shouldn't be able to see it. Please contact an admin so he can manage the command access."
+                    , allowed_mentions=None
                 )
+            case _:
                 self.bot.logger.error(
                     f"{message_beginning} - Error: {error}"
+                )
+                await ctx.respond(
+                    "An error occurred while executing the command. Please contact an admin."
                 )
                 raise error
 
