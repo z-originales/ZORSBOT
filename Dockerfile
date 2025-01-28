78010d7cf465e413bee21b3b94b8f52f379d3782
ZEN <41087061+Lolozendev@users.noreply.github.com>
2025-01-28 01:39:28 +0100

    Building a new way to handle assets and cogs, allowing for quick hot reload even with volume based cogs, startup script now check if base elements are present, it forces the user to put the volumes of the running bot in one same folder
    Remade docker file so it's splited in two images on to build the other is the main app, it allows me to take better and full advantage of the poetry tool
    
    updated github action in concequences

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,66 @@
-FROM python:3.13-slim-bookworm
+# BUILDER stage
+FROM python:3.13-slim-bookworm AS builder
 LABEL authors="ZEN"
 WORKDIR /app
 
+# Install Poetry
+RUN pip install poetry
+
+# Copy only the necessary files for Poetry to install dependencies
+COPY pyproject.toml poetry.lock ./
+
+ENV POETRY_NO_INTERACTION=1 \
+    POETRY_VIRTUALENVS_IN_PROJECT=1\
+    POETRY_VIRTUALENVS_CREATE=1\
+    POETRY_CACHE_DIR='/tmp/poetry_cache'
+
+# Install the dependencies
+RUN poetry install --no-root && rm -rf /tmp/poetry_cache
+
+# APP stage
+FROM python:3.13-slim-bookworm AS app
+LABEL authors="ZEN"
+WORKDIR /app
+
+RUN mkdir -p /backups
+
 RUN apt update && apt install libssl-dev -y
 
-RUN python -m venv .venv
+# Copy the virtual environment from the builder stage
+COPY --from=builder /app/.venv /app/.venv
+
+# Set the PATH to use the virtual environment
 ENV PATH="/app/.venv/bin:$PATH"
 
-COPY requirements.txt requirements.txt
-RUN pip install --no-cache-dir -Ur requirements.txt
+# set python unbuffered so that the output is printed to the console
+ENV PYTHONUNBUFFERED=1
 
-RUN mkdir assets
-COPY assets/ assets/
+# Set the environment variables for the application folder and backup folder
+ENV APP_FOLDER=/app
+ENV BACKUP_FOLDER=/backups
+
+# Copy the application files
+## volumes aimed folders
 RUN mkdir cogs
-COPY cogs/ cogs/
+RUN mkdir -p /backups
+COPY /cogs /backups/cogs
+COPY assets/ /backups/assets
 RUN mkdir logs
+
+## application files
 COPY prisma/ prisma/
 COPY utils/ utils/
 COPY main.py main.py
 
+## Volumes declaration
+VOLUME /app/cogs
+VOLUME /app/logs
+VOLUME /app/assets
+
+# Generate Prisma client , Generating it here to avoid generating it in every container and make the run faster
 RUN prisma generate
 
-ENTRYPOINT ["python", "main.py"]
-
-
+# Copy the entrypoint script
+COPY entrypoint.sh entrypoint.sh
+# Set the entrypoint
+ENTRYPOINT ["./entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+check_folder_content() {
+  # Checks if all files in foldera are present in folderb no matter their names
+  local foldera="$1"
+  local folderb="$2"
+
+  # Get sha512sum of all files in foldera (including subfolders)
+  local foldera_hashes=$(find "$foldera" -type f -exec sha512sum {} + | awk '{print $1,$2}')
+
+  # Get sha512sum of all files in folderb (including subfolders)
+  local folderb_hashes=$(find "$folderb" -type f -exec sha512sum {} + | awk -v folderb="$folderb" -v foldera="$foldera" '{gsub(folderb, foldera, $2); print $1,$2}')
+
+  # Check if all hashes from foldera are present in folderb
+  while IFS= read -r hash; do
+    if ! grep -q "$hash" <<<"$folderb_hashes"; then
+      local file=$(awk '{print $2}' <<<"$hash")
+      echo "$file"
+    fi
+  done <<<"$foldera_hashes"
+}
+
+checkup_routine() {
+  local signaturefolder="$1"
+  local destinationfolder="$2"
+
+  if [ -z "$(ls -A "$destinationfolder")" ]; then
+    echo "$destinationfolder is empty, copying from backup"
+    cp -r "$signaturefolder"/* "$destinationfolder"
+  else
+    echo "$destinationfolder folder is not empty, checking content"
+    missing_files=$(check_folder_content "$signaturefolder" "$destinationfolder")
+    if [ -n "$missing_files" ]; then
+      echo "Copying missing files from $signaturefolder to $destinationfolder"
+      while IFS= read -r file; do
+        src_file="$signaturefolder/${file#$signaturefolder}"
+        dest_file="$destinationfolder/${file#$signaturefolder}"
+        dest_dir=$(dirname "$dest_file")
+        mkdir -p "$dest_dir"
+        cp "$src_file" "$dest_file"
+      done <<<"$missing_files"
+    else
+      echo "All content is already in destination folder"
+    fi
+  fi
+}
+
+#pre check
+if [ -z "$BACKUP_FOLDER" ]; then
+  echo "BACKUP_FOLDER is not set" >&2
+  exit 1
+fi
+
+# Config variables
+backup_folder="$BACKUP_FOLDER"
+cogs_folder="$APPLICATION_FOLDER/cogs"
+assets_folder="$APPLICATION_FOLDER/assets"
+
+# Checkup routine on cogs and assets
+checkup_routine "$backup_folder/cogs" "$cogs_folder"
+checkup_routine "$backup_folder/assets" "$assets_folder"
+
+
+# push the database in the entrypoint so the prisma file has access to the env variables
+prisma db push
+
+# Run the application
+exec python3 main.py
