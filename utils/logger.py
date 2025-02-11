33656e0db16baa9e5b41f4e6758440847e281a68
ZEN <41087061+Lolozendev@users.noreply.github.com>
2025-02-11 01:01:52 +0100

    formating

--- utils/logger.py
+++ utils/logger.py
@@ -10,7 +10,7 @@ _default_event_level = "TRACE"
 _default_issue_level = "WARNING"
 
 
-def setup_logger(log_folder_path: str,level: str) -> None:
+def setup_logger(log_folder_path: str, level: str) -> None:
     logger.remove()
     add_event_console(_default_event_level)
     add_issue_console(_default_issue_level)
@@ -25,46 +25,44 @@ def add_event_console(log_level: str) -> None:
         format=_event_format,
         level=log_level,
         colorize=True,
-        filter=lambda record: record["level"].no < logger.level(_default_issue_level).no
+        filter=lambda record: record["level"].no
+        < logger.level(_default_issue_level).no,
     )
 
 
 def add_issue_console(log_level: str) -> None:
-    logger.add(
-        stderr,
-        format=_issue_format,
-        level=log_level,
-        colorize=True
-    )
+    logger.add(stderr, format=_issue_format, level=log_level, colorize=True)
 
 
-def add_event_log_file(log_level: str,log_folder_path: str) -> None:
+def add_event_log_file(log_level: str, log_folder_path: str) -> None:
     logger.add(
         log_folder_path + "/events/events.log",
         format=_event_format,
         rotation=_rotation_duration,
         retention=_retention_duration,
         level=log_level,
-        filter=lambda record: record["level"].no < logger.level(_default_issue_level).no,
-        compression=_compression_type
+        filter=lambda record: record["level"].no
+        < logger.level(_default_issue_level).no,
+        compression=_compression_type,
     )
 
 
-def add_issue_log_file(log_level: str,log_folder_path: str) -> None:
+def add_issue_log_file(log_level: str, log_folder_path: str) -> None:
     logger.add(
         log_folder_path + "/issues/issues.log",
         format=_issue_format,
         rotation=_rotation_duration,
         retention=_retention_duration,
         level=log_level,
-        compression=_compression_type
+        compression=_compression_type,
     )
 
+
 def set_colors() -> None:
-    logger.level("TRACE",color = "<magenta>")
-    logger.level("DEBUG",color = "<cyan>")
-    logger.level("INFO",color = "<blue>")
-    logger.level("SUCCESS",color = "<green>")
-    logger.level("WARNING",color = "<yellow>")
-    logger.level("ERROR",color = "<red>")
-    logger.level("CRITICAL",color = "<b><RED><white>")
+    logger.level("TRACE", color="<magenta>")
+    logger.level("DEBUG", color="<cyan>")
+    logger.level("INFO", color="<blue>")
+    logger.level("SUCCESS", color="<green>")
+    logger.level("WARNING", color="<yellow>")
+    logger.level("ERROR", color="<red>")
+    logger.level("CRITICAL", color="<b><RED><white>")
