ab150692f919e7dfdc1cd7ff8f6fd497e3fcaf07
ZEN <41087061+Lolozendev@users.noreply.github.com>
2025-03-11 01:27:23 +0100

    Refactoring bot backbone and database model
    - Refactored the bot design pattern :
     - Added a startup cog:
      - startup.py : it now manages all the bot startup logic
     - It now use a new setting/config system
     - made a color utility, added a singleton meta class, removed utilities.py,
    
    - The bot is now build around sqlmodel :
     - The database configuration, logic and schemas are in model/

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
+import traceback
+
 from typing import Self
 
 import discord
 from discord.ext import commands
-from utils import logger, utilities
-from loguru import logger as log
-from prisma import Prisma
-from asyncio import run
+from typing_extensions import override
 
+from utils import logger
+from loguru import logger as log
+from asyncio import run
+from config.settings import settings
+from model.database import Database
 
 class ZORS(commands.Bot):
-    def __init__(self, env_vars=None, *args, **kwargs):
+    database: Database
+    main_guild: discord.Guild | None
+
+    def __init__(self, *args, **kwargs):
         log.debug("ZORS bot is starting up...")
         super().__init__(*args, **kwargs)
-        self.envs: dict[str, str] = env_vars
-        self.database: Prisma = Prisma()
-        log.info("Succesfully connected to the database")
+        self.database: Database = Database(str(settings.postgres_url))
+        self.main_guild = None
+        log.info("Successfully connected to the database")
         log.trace("ZORS bot has been initialized.")
-        log.info("ZORS bot is ready to go.")
-        log.debug("Loading cogs...")
+        log.info("Loading cogs...")
         self._load_cogs()
 
     @classmethod
@@ -45,35 +51,21 @@ class ZORS(commands.Bot):
             intents=zorsintents,
             help_command=None,
         )
-        await bot.database.connect()
+        await bot.database.create_db_and_tables()
         return bot
 
-    async def check_for_new_users(self) -> bool:
+    @override
+    async def start(self, *args, **kwargs) -> None:
         """
-        Checks for new users in the guild and adds them to the database if they are not already in it.
-        :return: bool - True if new users were added, False if no new users were added.
-        """
-        new_users = False
-        for guild in self.guilds:
-            for member in [member for member in guild.members if not member.bot]:
-                if not await self.database.user.find_unique(where={"id": member.id}): #TODO: be careful when a user changes his username (maybe there is a listener for that)
-                    new_users = True
-                    await self.database.user.create(
-                        data={"id": member.id, "name": member.name, "type": "COMMON_USER"}
-                    )
-        return new_users
-
-    async def on_ready(self):
-        """
-        Event that is called when the bot is ready.
+        Starts the bot with the token from the settings.
         Returns:
 
         """
-        log.debug("ZORS bot is up and ready.")
-        log.trace(f"Logged in as {self.user} ({self.user.id})")
-        log.debug("Checking for new users...")
-        if await self.check_for_new_users():
-            log.info("Added new users to the database.")
+        await super().start(settings.discord_token, *args, **kwargs)
+
+
+
+
 
     def _load_cogs(self) -> None:
         """
@@ -100,25 +92,16 @@ class ZORS(commands.Bot):
                     log.error(f"Cog failed to load: {extension} - {status[extension]}")
                 case _:
                     log.error(f"Unknown error: {extension} - {status[extension]}")
-
+                    print(traceback.format_exc())
 
 @log.catch(
     level="CRITICAL",
     message="Unexpected error occurred, that forced the bot to shut down.",
 )
-async def main() -> ZORS:
-    try:
-        env_vars = utilities.get_required_env_vars()
-        logger.setup_logger(
-            "logs" if "LOGS_PATH" not in env_vars else env_vars["LOGS_PATH"], "DEBUG"
-        )
-    except EnvironmentError as e:
-        log.critical(f"Failed to start the bot: {e}")
-        exit(1)
-
+async def main():
+    logger.setup_logger('logs' if not settings.logs_path else settings.logs_path, 'INFO' if not settings.log_level else settings.log_level)
     zors_bot = await ZORS.create_bot()
-    await zors_bot.start(env_vars["DISCORD_TOKEN"])
-
+    await zors_bot.start()
 
 if __name__ == "__main__":
     run(main())
---
+++ model/database.py
@@ -0,0 +1,17 @@
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import SQLModel
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from utils.singletonmeta import SingletonMeta
+
+
+class Database(metaclass=SingletonMeta):
+    def __init__(self, url: str):
+        self.engine = create_async_engine(url)
+        self.sessionmaker = async_sessionmaker(self.engine, class_=AsyncSession, expire_on_commit=False)
+
+    async def create_db_and_tables(self):
+        async with self.engine.begin() as conn:
+            await conn.run_sync(SQLModel.metadata.create_all)
+
+    def get_session(self) -> AsyncSession:
+        return self.sessionmaker()
